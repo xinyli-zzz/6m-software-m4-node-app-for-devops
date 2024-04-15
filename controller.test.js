021a3e574d623a37bba7280fbd47c3009dde8b40
Xinyli <xinyuu98@gmail.com>
2024-04-15 20:39:46 +0800

    make test fail

--- controller.test.js
+++ controller.test.js
@@ -14,7 +14,7 @@ describe("the print function", ()=>{
         }
 
         targetFn(mockReq, mockRes);
-        expect(mockRes.send).toHaveBeenCalledWith("Hello world!!"); // If you change this value, the test will fail.
+        expect(mockRes.send).toHaveBeenCalledWith("Hello world!!!"); // If you change this value, the test will fail.
     })
 })
 
